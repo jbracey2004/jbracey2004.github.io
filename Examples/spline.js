613aeae74bd9b66eda374189e9511b781fd81b72
Jason Bracey <jbracey2004@gmail.com>
2018-02-12 18:26:48 -0500

    splinejs WIP - 201802121820

--- Examples/spline.js
+++ Examples/spline.js
@@ -1,3 +1,4 @@
+var SplineEndBehaviour = {Halt: 0, Loop: 1, ContinueTan: 2, ContinuePattern: 3};
 function splineNode(setPos, setTangentPre, setTangentPost)
 {
 	this.Pos = setPos;
@@ -15,21 +16,86 @@ function spline(setNodes)
 spline.prototype.NetDifference = function() {
 	return vecN.diff(this.nodes[this.nodes.length - 1].Pos, this.nodes[0].Pos);
 }
-spline.prototype.MapSplinePosToNodeIndex = function(splinePos) {
-	var fPos = splinePos;
-	while(fPos<0) {fPos += 1;}
-	return (fPos % 1) * this.nodes.length;
+spline.prototype.MapSplinePosToNodeIndex = function (splinePos) {
+	return (splinePos * this.nodes.length);
 }
-spline.prototype.MapNodeIndexToSplinePos = function(nodeIndex) {
-	var idxPos = nodeIndex;
-	while(idxPos < 0) {idxPos += this.nodes.length;}
-	return (idxPos/this.nodes.lengt) % 1;
+spline.prototype.MapNodeIndexToSplinePos = function (nodeIndex) {
+	return (nodeIndex) / (this.nodes.length);
+}
+spline.prototype.AdjustNodeIndexToEndBehaviour = function(nodeIndex, intSplineEndBehaviour) {
+	var fIdx = nodeIndex;
+	var NLen = this.nodes.length;
+	if (intSplineEndBehaviour === SplineEndBehaviour.Halt || 
+		intSplineEndBehaviour === SplineEndBehaviour.ContinueTan) {
+		if (fIdx < 0) { fIdx = 0; }
+		if (fIdx > NLen - 1) { fIdx = NLen - 1; }
+	}
+	if (intSplineEndBehaviour === SplineEndBehaviour.Loop ||
+		intSplineEndBehaviour === SplineEndBehaviour.ContinuePattern) {
+		if (fIdx < 0) { fIdx += ((1 + floor(-fIdx / NLen)) * NLen); }
+		if (fIdx > NLen - 1) { fIdx -= ((floor(fIdx / NLen)) * NLen); }
+	}
+	return fIdx;
 }
 spline.prototype.addNodes = function(newNodes) {
 	for (var i = 0; i < setNodes.length; i++) { this.nodes.push(setNodes[i]); }
 }
-spline.prototype.removeNode = function(idxNode, intCount) {
+spline.prototype.removeNodesAt = function(idxNode, intCount) {
 	this.nodes.splice(idxNode, intCount);
+}
+spline.prototype.PosAt = function(fnodePos, intSplineEndBehaviour) {
+	var NLen = this.nodes.length;
+	var fPos = this.MapSplinePosToNodeIndex(fnodePos);
+	var fPosSt = floor(fPos);
+	var fPosEd = fPosSt + 1;
+	var fPosT = fPos - fPosSt;
+	var vecRet = new vecN();
+	if(fPos >= 0 && fPos <= NLen - 1) {
+		var nodeSt = this.nodes[fPosSt];
+		var nodeEd = this.nodes[fPosEd];
+		vecRet = spline.interpolate(fPosT, nodeSt.Pos, nodeSt.PostTan, nodeEd.Pos, nodeEd.PreTan);
+	} else {
+		switch (intSplineEndBehaviour) {
+			case SplineEndBehaviour.Halt:
+				if(fPos < 0) vecRet = this.nodes[0].Pos;
+				if(fPos > NLen - 1) vecRet = this.nodes[NLen - 1].Pos;
+				break;
+			case SplineEndBehaviour.ContinueTan:
+				var extrap = 0;
+				if (fPos < 0) {
+					extrap = int(-fPos);
+					vecRet = vecN.sum(this.nodes[0].Pos, vecN.scale(this.nodes[0].TanPre, extrap));
+				} else if (fPos > NLen - 1) {
+					extrap = int(fPos - (NLen - 1));
+					vecRet = vecN.sum(this.nodes[NLen].Pos, vecN.scale(this.nodes[NLen].TanPost, extrap));
+				}
+				break;
+			case SplineEndBehaviour.ContinuePattern:
+				var extrap = 0;
+				if (fPos < 0) {
+					extrap = int(-fPos);
+					vecRet = vecN.sum(this.nodes[0].Pos, vecN.scale(this.NetDifference(), -extrap));
+				} else if (fPos > NLen - 1) {
+					extrap = int(fPos - (NLen - 1));
+					vecRet = vecN.sum(this.nodes[NLen].Pos, vecN.scale(this.NetDifference(), extrap));
+				}
+				intSplineEndBehaviour = SplineEndBehaviour.Loop;
+			case SplineEndBehaviour.Loop:
+				if(fPos < 0) {
+					fPos += ((1 + floor(-fPos / NLen)) * NLen);
+				} else if(fPos > NLen - 1) {
+					fPos -= ((floor(fPos / NLen)) * NLen);
+				}
+				fPosSt = floor(fPos);
+				fPosEd = (fPosSt + 1) % (NLen);
+				fPosT = fPos - fPosSt;
+				var nodeSt = this.nodes[fPosSt];
+				var nodeEd = this.nodes[fPosEd];
+				vecRet = vecN.sum(vecRet, spline.interpolate(fPosT, nodeSt.Pos, nodeSt.PostTan, nodeEd.Pos, nodeEd.PreTan));
+				break;
+		}
+	}
+	return vecRet;
 }
 spline.interpolate = function(fPos, vecPos1, vecTan1, vecPos2, vecTan2) {
 	var vecRet = new vecN();
